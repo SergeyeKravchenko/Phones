57fed2b85a1c92a8130493cc0038820341780d1b
Sergey <sergeye.kravchenko@gmail.com>
2018-02-03 00:37:07 +0300

    refactoring test

--- src/test/java/com/example/PhonesApplicationTests.java
+++ src/test/java/com/example/PhonesApplicationTests.java
@@ -7,13 +7,23 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import javax.servlet.Filter;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.FormLoginRequestBuilder;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -26,25 +36,41 @@ public class PhonesApplicationTests {
     @Autowired
     private WebApplicationContext context;
 
+    @Autowired
+    private Filter springSecurityFilterChain;
+
     private MockMvc mockMvc;
 
     @Before
     public void setup() {
         mockMvc = MockMvcBuilders
                 .webAppContextSetup(context)
-                .apply(springSecurity())
+                .addFilters(springSecurityFilterChain)
                 .build();
+    }
+
+    @Test
+    public void loginWithValidUserThenAuthenticated() throws Exception {
+        FormLoginRequestBuilder login = formLogin()
+                .user("admin")
+                .password("admin");
+        mockMvc.perform(login)
+                .andExpect(authenticated().withUsername("admin"));
+    }
+
+    @Test
+    public void loginWithInvalidUserThenUnauthenticated() throws Exception {
+        FormLoginRequestBuilder login = formLogin()
+                .user("invalid")
+                .password("invalidpassword");
+
+        mockMvc.perform(login)
+                .andExpect(unauthenticated());
     }
 
     @Test
     public void accessLoginThenOk() throws Exception {
         mockMvc.perform(get("/login"))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    public void accessRegistrationThenOk() throws Exception {
-        mockMvc.perform(get("/registration"))
                 .andExpect(status().isOk());
     }
 
@@ -81,6 +107,19 @@ public class PhonesApplicationTests {
         mockMvc.perform(get("/updatefield"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrlPattern("**/login"));
+    }
+
+    @Configuration
+    @EnableWebSecurity
+    @EnableWebMvc
+    static class Config extends WebSecurityConfigurerAdapter {
+
+        @Autowired
+        public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+            auth
+                    .inMemoryAuthentication()
+                    .withUser("admin").password("admin").roles("USER");
+        }
     }
 
 }
